715e308e7b46f06b0b1a88e1791cb0455f682d32
Rajveer Singh <rajsidhu2924@gmail.com>
2023-03-30 15:43:13 -0700

    server file config and server setup

--- server/index.js
+++ server/index.js
@@ -4,3 +4,23 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
+
+// configrations
+
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// Server setup
+
+const PORT = process.env.PORT || 9000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+}) 
