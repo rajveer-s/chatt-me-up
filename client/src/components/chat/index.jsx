add9dcd1d20a91ad3182330be062f6f52cc72045
Rajveer Singh <rajsidhu2924@gmail.com>
2023-06-24 16:33:08 -0700

    ai component called in index chat

--- client/src/components/chat/index.jsx
+++ client/src/components/chat/index.jsx
@@ -1,31 +1,37 @@
-import React from 'react'
-import {useMultiChatLogic,
-MultiChatSocket,
-MultiChatWindow,} from "react-chat-engine-advanced";
-import CustomHeader from '../customHeader';
-import StandardMessageForm from '../customMessageForm/StandardMessageForm';
+import React from "react";
+import {
+  useMultiChatLogic,
+  MultiChatSocket,
+  MultiChatWindow,
+} from "react-chat-engine-advanced";
+import CustomHeader from "../customHeader";
+import StandardMessageForm from "../customMessageForm/StandardMessageForm";
+import Ai from "../customMessageForm/Ai";
 
 const Chat = () => {
-    const chatProps = useMultiChatLogic(
-        import.meta.env.VITE_PROJECT_ID,
-        "test",
-        "1234"
-    )
+  const chatProps = useMultiChatLogic(
+    import.meta.env.VITE_PROJECT_ID,
+    "user1",
+    "1234"
+  );
   return (
-    <div style={{flexBasis: "100%"}}>
-        <MultiChatSocket {...chatProps}/>
-        <MultiChatWindow 
+    <div style={{ flexBasis: "100%" }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow
         {...chatProps}
         style={{ height: "100vh" }}
         renderChatHeader={(chat) => <CustomHeader chat={chat} />}
         renderMessageForm={(props) => {
-            return (
-                <StandardMessageForm props={props} activeChat={chatProps.chat} />
-            )
+          if (chatProps.chat?.title.startsWith("AiChat_")) {
+            return <Ai props={props} activeChat={chatProps.chat} />;
+          }
+          return (
+            <StandardMessageForm props={props} activeChat={chatProps.chat} />
+          );
         }}
-        />
-        </div>
-  )
-}
+      />
+    </div>
+  );
+};
 
-export default Chat
+export default Chat;
